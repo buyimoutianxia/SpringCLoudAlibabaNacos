d4bd88e930d31d07b2c1e0042ceb5d8bace0d6b6
buyimoutianxia <buyimoutianxia@126,com>
2020-04-23 16:57:32 +0800

    Nacos 服务提供者

---
+++ NacosProvider-3001/src/main/java/com/xyz/Provider/ProviderApplication.java
@@ -0,0 +1,22 @@
+package com.xyz.Provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xyz
+ * @date 2020-04-23-14:36
+ * @decription Nacos Provider启动类
+ * {@link EnableDiscoveryClient}开启Nacos的服务发现
+ */
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
---
+++ NacosProvider-3001/src/main/java/com/xyz/Provider/controller/ProviderController.java
@@ -0,0 +1,24 @@
+package com.xyz.Provider.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xyz
+ * @date 2020-04-23-15:04
+ * @decription Nacos服务提供者控制层
+ */
+
+@RestController
+public class ProviderController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/provider/{path}")
+    public String demo(@PathVariable(value = "path") String path) {
+       return "Nacos Provider path:" + path + ",port:" + port ;
+    }
+}
